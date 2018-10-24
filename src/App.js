e8d6586ab25d6401a33fa339a45ddf3e04d94f9f
Lukas <devmojoo@gmail.com>
2018-10-24 13:48:00 +0300

    ReactJS web app. Improved error checks.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,97 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-class App extends Component {
+import Titles from "./components/Titles";
+import Form from "./components/Form";
+import Weather from "./components/Weather";
+
+const API_KEY = "ca561fa56f4fa6d521769011281bd881";
+
+class App extends React.Component {
+  state = {
+     temperature : undefined,
+     city : undefined,
+     country : undefined,
+     humidity : undefined,
+     description : undefined,
+     error : undefined
+  }
+
+  getWeather = async (e) => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+
+    if ( city && country ) {
+      try {
+        const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
+        const data = await api_call.json(); 
+
+        console.log(data); 
+
+        this.setState({
+          temperature : data.main.temp,
+          city : data.name,
+          country : data.sys.country,
+          humidity : data.main.humidity,
+          description : data.weather[0].description,
+          error : ""
+        });
+      }
+      catch(e) {
+        this.setState({
+          temperature : undefined,
+          city : undefined,
+          country : undefined,
+          humidity : undefined,
+          description : undefined,
+          error : "Please enter correct city and country."
+        });
+      }
+    } 
+    else {
+      this.setState({
+        temperature : undefined,
+        city : undefined,
+        country : undefined,
+        humidity : undefined,
+        description : undefined,
+        error : "Please enter the values. ",
+      });
+    }
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+    return(
+      <div>
+        <div className="wrapper">
+          <div className="main">
+            <div className="">
+              <div className="row">
+                <div className="col-xs-5 title-container">
+                  <Titles />
+                </div>
+                <div className="col-xs-7 form-container">
+                    <Form getWeather={this.getWeather}/>
+                    <Weather 
+                      temperature = {this.state.temperature }
+                      city = {this.state.city }
+                      country = {this.state.country }
+                      humidity = {this.state.humidity }
+                      description = {this.state.description }
+                      error = {this.state.error }
+                    /> 
+                  </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 }
 
 export default App;
+
+{/* <Titles />
+
+*/}
